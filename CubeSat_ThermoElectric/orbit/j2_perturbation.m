ab8e6d542ae6459ea41f8ad22d8a91c8c57f2fa9
Doitordead <dictionnary15@gmail.com>
2026-09-10 15:25:33 +0200

    [#2] Implement J2 perturbation

--- CubeSat_ThermoElectric/orbit/j2_perturbation.m
+++ CubeSat_ThermoElectric/orbit/j2_perturbation.m
@@ -26,13 +26,13 @@ function [pr, ps, pw] = j2_perturbation(r, i, om, theta, mu, R_E, J2)
 % Argument de latitude (om + theta), recurrent dans les 3 formules.
 u = om + theta;
 
-% --- Eq.18a : composante radiale -------------------------------------------
+% --- Eq.18a : composante radiale -----------------------------------------
 pr = -1.5 * (J2*mu*R_E^2/r^4) * (1 - 3*sin(i)^2*sin(u)^2);
 
-% --- Eq.18b : composante transverse (s_hat) ---------------------------------
+% --- Eq.18b : composante transverse (s_hat) ------------------------------
 ps = -1.5 * (J2*mu*R_E^2/r^4) * sin(i)^2 * sin(2*u);
 
-% --- Eq.18c : composante normale (w_hat) ------------------------------------
+% --- Eq.18c : composante normale (w_hat) ---------------------------------
 pw = -1.5 * (J2*mu*R_E^2/r^4) * sin(2*i) * sin(u);
 
 end
